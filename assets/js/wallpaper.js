a3b2d4924cd9575edc1db6ca8b4ffa089cc91c4b
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-02-03 16:35:27 +0530

    update wallpaper.js

--- assets/js/wallpaper.js
+++ assets/js/wallpaper.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
     $('.menu-toggle').click(function(){
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
+        $("body").toggleClass("fixed");
     })
 })
