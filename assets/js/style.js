1df67ccc4d64a449c34203a15e38fe9ff6c3861d
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-02-03 16:32:14 +0530

    update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('.menu-toggle').click(function(){
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
-        $("body").toggleClass("fixed")
+        $("body").toggleClass("fixed");
     })
 })
