7d36bfe1ff35d468d72466112c250fa1248302bb
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-02-16 21:16:14 +0530

    Delete assets directory

--- assets/js/wallpaper.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.menu-toggle').click(function(){
-        $(this).toggleClass("active");
-        $(".navbar-menu").toggleClass("active");
-        $("body").toggleClass("fixed");
-    })
-})
